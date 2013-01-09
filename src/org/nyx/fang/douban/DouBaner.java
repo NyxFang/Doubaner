f5da9dd9098c5cc869832b0e701bd626957b6f1f
thr.stone <thr.stone@gmail.com>
2013-01-09 12:43:33 +0800

    init

---
+++ src/org/nyx/fang/douban/DouBaner.java
@@ -0,0 +1,13 @@
+package org.nyx.fang.douban;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DouBaner extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
