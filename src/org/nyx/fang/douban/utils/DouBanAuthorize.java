e72d3641a687b1b83e3815121628e099e15e20ab
Nyx.F <thr.stone@gmail.com>
2013-01-15 09:57:28 +0800

    login fininsh

--- src/org/nyx/fang/douban/utils/DouBanAuthorize.java
+++ src/org/nyx/fang/douban/utils/DouBanAuthorize.java
@@ -8,6 +8,7 @@ package org.nyx.fang.douban.utils;
  *
  */
 
+import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,9 +17,10 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
 
 import android.util.Log;
 public class DouBanAuthorize {
@@ -30,7 +32,7 @@ public class DouBanAuthorize {
 	
 	private final String SECRET = "c75d81742eee6eb1";
 	
-	private final String CALLBACK = "http://mobileApp.com";
+	private final String CALLBACK = "http://mobileapp.com";
 	
 	private final String _URL = "https://www.douban.com/service/auth2/auth";
 	
@@ -40,39 +42,59 @@ public class DouBanAuthorize {
 	
 	private final String TOKEN_EXPIRED = "access_token_has_expired";
 	
+	private String authCode = "";
+	
+	private String authInfo = "";
+	
 	public DouBanAuthorize() {
 		
 	}
 	
 	public String authURL(){
 		
-//		return _URL+"?"+"client_id="+API_KEY+"&"+"redirect_uri="+CALLBACK+"&"+"response_type=code&scope=shuo_basic_r,shuo_basic_w,douban_basic_common";
-		return "https://www.douban.com/service/auth2/auth?client_id=09eab9b614fb26761dc02d223ef17362&redirect_uri=http://mobileApp.com&response_type=code&scope=shuo_basic_r,shuo_basic_w,douban_basic_common";
+		return _URL+"?"+"client_id="+API_KEY+"&"+"redirect_uri="+CALLBACK+"&"+"response_type=code&scope=shuo_basic_r,shuo_basic_w,douban_basic_common";
+//		return "https://www.douban.com/service/auth2/auth?client_id=09eab9b614fb26761dc02d223ef17362&redirect_uri=http://mobileApp.com&response_type=code&scope=shuo_basic_r,shuo_basic_w,douban_basic_common";
 	}
 	
 	public String tokenURL(){
 		return _tokenURL;
 	}
 	
-	public void getToken(String code){
+	
+	
+	public String getToken(String code){
+		authCode = code;
+		new Thread(){
+			@Override
+			public void run(){
+				List<NameValuePair> params = new ArrayList<NameValuePair>();
+				params.add(new BasicNameValuePair("client_id", API_KEY));
+				params.add(new BasicNameValuePair("client_secret", SECRET));
+				params.add(new BasicNameValuePair("redirect_uri", CALLBACK));
+				params.add(new BasicNameValuePair("grant_type", "authorization_code"));
+				params.add(new BasicNameValuePair("code", authCode));
+				
+				//create post link;
+				HttpPost httppost = new HttpPost(tokenURL());
+				try{
+					Log.d("Debug","post url:"+tokenURL());
+					// set request;
+					httppost.setEntity(new UrlEncodedFormEntity(params,HTTP.UTF_8));
+					// get response;
+					HttpResponse res = new DefaultHttpClient().execute(httppost);
+					Log.d("Debug","statusCode:"+res.getStatusLine().getStatusCode());
+					if(res.getStatusLine().getStatusCode() == 200){
+						authInfo=EntityUtils.toString(res.getEntity());
+					}
+					
+				}catch (Exception e) {
+					// TODO: handle exception
+					Log.d("Debug","error"+e);
+				}
+			}
+		}.start();
 		
-		try {
-            List<NameValuePair> params = new ArrayList<NameValuePair>();
-            params.add(new BasicNameValuePair("client_id", API_KEY));
-            params.add(new BasicNameValuePair("client_secret", SECRET));
-            params.add(new BasicNameValuePair("redirect_uri", CALLBACK));
-            params.add(new BasicNameValuePair("grant_type", "authorization_code"));
-            params.add(new BasicNameValuePair("code", code));
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpPost httppost = new HttpPost(tokenURL());
-            httppost.setEntity(new UrlEncodedFormEntity(params));
-            HttpResponse response = httpclient.execute(httppost);
-            String data = new BasicResponseHandler().handleResponse(response);            
-            Log.d("Debug","test"+data);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-		
+		return authInfo;
 	}
 	
 }
